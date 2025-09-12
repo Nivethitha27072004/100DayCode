05725de629bc0e58a382b3fb9c8911d58d178d89
Nivethitha27072004 <nivevel2704@gmail.com>
2025-09-12 22:48:15 +0530

    first commit

---
+++ src/PositiveOrNegative.java
@@ -0,0 +1,10 @@
+
+public class PositiveOrNegative{
+    public static void main(String[] args) {
+        int number=34;
+        if(number<0) System.err.println("negative");
+        else System.err.println("postive");
+
+    }
+
+}
